8c6d8a1323c48ef65aee7b4bb3039ff6ff623b18
Aniket Yadav <aniket912yadav@gmail.com>
2024-06-02 13:23:47 +0530

    Created models + routes

---
+++ config/mongoose-connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/elite")
+    .then(function () {
+        console.log("Connected!");
+    })
+    .catch(function (err) {
+        console.log(err.message);
+    })
+
+
+module.exports = mongoose.connection;
